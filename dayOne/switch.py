e7d4e4bc7c21b5fd639965424d0d992abfcedc31
Yanis <yanismehalaine@outlook.fr>
2022-07-10 20:19:07 +0200

    switch

---
+++ dayOne/switch.py
@@ -0,0 +1,17 @@
+
+a = input("Enter a number : ")
+b = input("Enter a number : ")
+
+
+
+print("a = " + a)
+print("b = " + b)
+
+print("Now let's switch : ")
+
+c = a
+a = b
+b = c
+
+print("a = " + a)
+print("b = " + b)
