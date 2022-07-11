358ffa774a0c3b5280c46829fd70138f3c6a009f
Yanis <yanismehalaine@outlook.fr>
2022-07-12 00:06:30 +0200

    init day eight

--- 008/start.py
+++ 008/start.py
@@ -1,2 +1,7 @@
-def greet():
+def greet(name):
+    print(f"Hello {name}")
+    print("How do you do ?")
+    print("Isn't the weather nice today?")
     
+n = input("What's your name ? ") 
+greet(n)
