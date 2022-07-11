78daabe26e14e89347d160e967204beedd98f73f
Yanis <yanismehalaine@outlook.fr>
2022-07-12 00:31:14 +0200

    end of day eight - minor changes

--- 008/cipher.py
+++ 008/cipher.py
@@ -1,22 +1,21 @@
-def encrypte(string, key):
+def cypher(string, key, encrypting):
     result = ""
     for char in string:
-        result += chr(ord(char) + key)
+        if encrypting:
+            result += chr(ord(char) + key)
+        else:
+            result += chr(ord(char) - key)
     return result
 
-def decrypte(string, key):
-    result = ""
-    for char in string:
-        result += chr(ord(char) - key)
-    return result
 
+#Initialisation
 message = input("Give a message to encrypte : ")
 key = int(input("Give a key to go with : "))
 
-encryptedMessage = encrypte(message, key)
+#Function call
+encryptedMessage = cypher(message, key, True)
+decryptedMessage = cypher(encryptedMessage, key, False)
 
+#Display
 print(f"Your encrypted message : {encryptedMessage}")
-
-decryptedMessage = decrypte(encryptedMessage, key)
-
 print(f"Your decrypted message : {decryptedMessage}")
