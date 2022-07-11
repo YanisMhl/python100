f7a37ee525c353c49e78134a9059de16cc52da93
Yanis <yanismehalaine@outlook.fr>
2022-07-12 00:13:02 +0200

    done the paint challenge

---
+++ 008/paint.py
@@ -0,0 +1,11 @@
+def canNumber(height, width, coverage):
+    return round((height*width)/coverage, 1) 
+
+
+h = int(input("What's the height of your wall ? "))
+w = int(input("What's the width of your wall ? "))
+c = int(input("What's the coverage of your cans ? "))
+
+number = canNumber(h, w, c)
+
+print(f"The number of cans you need is : {number}")
