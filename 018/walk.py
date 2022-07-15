90ec2e7ce0a5a5c7f3eb2c7f28b218251b7a47c2
Yanis <yanismehalaine@outlook.fr>
2022-07-15 02:28:55 +0200

    finished walk project

---
+++ 018/walk.py
@@ -0,0 +1,19 @@
+import random
+from turtle import Turtle, Screen 
+
+colors = ["cornflower blue", "teal", "peach puff","light green", "purple"]
+
+turtle = Turtle()
+turtle.speed(0)
+directions = [90, 180, 270, 360]
+
+for i in range(1000):
+    turtle.color(random.choice(colors))
+    turtle.pensize(random.randint(3, 6))
+    turtle.forward(random.randint(20, 60))
+    turtle.right(random.choice(directions))
+
+
+#Screen
+screen = Screen()
+screen.exitonclick()
