89dc1a65b094cfba668533c712637d8b6ec3b8cc
Yanis <yanismehalaine@outlook.fr>
2022-07-15 03:34:02 +0200

    finished spirogram

---
+++ 018/spiro.py
@@ -0,0 +1,16 @@
+import random
+from turtle import Turtle, Screen 
+
+
+#Turtle
+t = Turtle()
+t.speed(10)
+
+for i in range(100):
+    t.circle(100)
+    t.setheading(t.heading() + 10)
+    
+
+#Screen
+screen = Screen()
+screen.exitonclick()
--- 018/walk.py
+++ 018/walk.py
@@ -5,11 +5,12 @@ colors = ["cornflower blue", "teal", "peach puff","light green", "purple"]
 
 turtle = Turtle()
 turtle.speed(0)
-directions = [90, 180, 270, 360]
+directions = [0, 90, 180, 270]
+
 
 for i in range(1000):
     turtle.color(random.choice(colors))
-    turtle.pensize(random.randint(3, 6))
+    turtle.pensize(random.randint(5, 10))
     turtle.forward(random.randint(20, 60))
     turtle.right(random.choice(directions))
 
