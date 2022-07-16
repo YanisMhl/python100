311f7f7eefcc19654d479c9eb002b6af451e2668
YanisMhl <yanismehalaine@outlook.fr>
2022-07-16 07:32:51 +0200

    started the race

--- 019/main.py
+++ 019/main.py
@@ -1,9 +1,21 @@
+import random
 from turtle import Turtle, Screen 
 
-tim = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
+userBet = screen.textinput(title="Make your bet", prompt="which turtle will win the race? Enter a color:")
 
+positions = [180, 60, -60, -180]
+
+tim = Turtle()
+tom = Turtle()
+tam = Turtle()
+tim.penup()
+tom.penup()
+tam.penup()
+tim.goto(x=-240, y=random.choice(positions))
+tom.goto(x=-240, y=random.choice(positions))
+tam.goto(x=-240, y=random.choice(positions))
 
 
 screen.exitonclick()
