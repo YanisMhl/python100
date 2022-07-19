6a6a4faeb126de623581955a0980b62fea8b6c9e
Yanis <yanismehalaine@outlook.fr>
2022-07-19 21:56:17 +0200

    updated day 25

--- 025/main.py
+++ 025/main.py
@@ -1,15 +1,21 @@
 import csv 
+import pandas 
 
 # data = []
 # with open("weather_data.csv") as file:
 #     data = file.readlines()
 # print(data)
 
-with open("weather_data.csv") as dataFile:
-    data = csv.reader(dataFile)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(row[1])
+# with open("weather_data.csv") as dataFile:
+#     data = csv.reader(dataFile)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(row[1])
 
-print(temperatures)
+# print(temperatures)
+
+data = pandas.read_csv("weather_data.csv")
+
+data_dict = data.to_dict()
+print(data_dict)
