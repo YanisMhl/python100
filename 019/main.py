e2dce9e468ee2f35c3a71f0c2fb15d2db28e0cc4
Yanis <yanismehalaine@outlook.fr>
2022-07-19 09:58:15 +0200

    change

--- 019/main.py
+++ 019/main.py
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-from turtle import Turtle, Screen
-
-
-tim = Turtle()
-screen = Screen()
-
-def move_forwards():
-    tim.forward(10)
-
-def move_backwards():
-    tim.backward(10)
-    
-def rotate_left():
-    tim.left(20)
-
-def rotate_right():
-    tim.right(20)
-    
-def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
-
-screen.listen()
-screen.onkey(key="z", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="q", fun=rotate_left)
-screen.onkey(key="d", fun=rotate_right)
-screen.onkey(key="c", fun=clear)
-=======
 import random
 from turtle import Turtle, Screen 
 
@@ -49,6 +17,5 @@ tim.goto(x=-240, y=random.choice(positions))
 tom.goto(x=-240, y=random.choice(positions))
 tam.goto(x=-240, y=random.choice(positions))
 
->>>>>>> 311f7f7eefcc19654d479c9eb002b6af451e2668
 
 screen.exitonclick()
