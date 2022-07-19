4ab9b1e4be1b1388675f0cc64d8143acf741540b
Yanis <yanismehalaine@outlook.fr>
2022-07-19 20:45:41 +0200

    learned about file editing

---
+++ 024/main.py
@@ -0,0 +1,7 @@
+# with open("myFile.txt") as file:
+#     content = file.read()
+#     print(content)
+
+with open("newFile.txt", mode="w") as file:
+    file.write("Ceci est un nouveau fichier.")
+    
