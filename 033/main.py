08604719c3df7e10cc907f5504705158810ba2cd
Yanis <yanismehalaine@outlook.fr>
2022-07-22 08:23:17 +0200

    ?

---
+++ 033/main.py
@@ -0,0 +1,9 @@
+import requests #type: ignore 
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+
+# data = response.json()
+# print(data)
+
+response = requests.get(url="https://api.sunrise-sunset.org/json")
+print(response.status_code)
--- 033/start.py
+++
@@ -1,6 +0,0 @@
-import requests #type: ignore 
-
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-
-data = response.json()
-print(data)
