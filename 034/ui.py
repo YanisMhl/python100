f1d0fd4f2607fb1083a3243b4e5e6151f992ca0c
Yanis <yanismehalaine@outlook.fr>
2022-07-29 20:17:42 +0200

    finished day 34

--- 034/ui.py
+++ 034/ui.py
@@ -7,7 +7,14 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        
+        self.scoreLbl = Label(text="Score : 0", fg="white", bg=THEME_COLOR)
+        self.scoreLbl.grid(row=0, column=1)
+        
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.questoin_text = self.canvas.create_text(150, 125, text="Some question text", fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2)
         
         
-
         self.window.mainloop()
